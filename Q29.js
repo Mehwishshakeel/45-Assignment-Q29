2f52c1d6b30005ce57d18adc7ce76c352df81509
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-21 14:17:06 +0500

    GitHub

---
+++ Q29.js
@@ -0,0 +1,19 @@
+// creat an arrauy of favourite_fruits
+let favourite_fruits = ["Banana", "Orange", "Kiwi"];
+//  using five if independent statement:
+if (favourite_fruits.includes("Banana")) {
+    console.log("I like Banana");
+}
+if (favourite_fruits.includes("Apple")) {
+    console.log("I dont like Apple");
+}
+if (favourite_fruits.includes("Orange")) {
+    console.log("Most favourite fruit is Orange");
+}
+if (favourite_fruits.includes("Kiwi")) {
+    console.log("delicious fruit is kiwi");
+}
+if (favourite_fruits.includes("Watarmelon")) {
+    console.log("not really like watarmelon");
+}
+export {};
